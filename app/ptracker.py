edf7758510b471c8d509d2d9961d586c47f9549e
Kenneth <khchong2011@yahoo.com>
2020-10-23 16:38:14 -0700

    moved under ptracker.py. Path changed to /addrow

--- app/ptracker.py
+++ app/ptracker.py
@@ -1,9 +1,34 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-# TODO: only import `engine` and 1 table
-from db import *
+from db import engine, Pageloads
+from sqlalchemy.orm import sessionmaker
+from datetime import datetime
 
 # Add URL handling functions here
 # API handlers should probably go under /api/
 # tracking shout probably go under something else
+
+# TODO: Input company name
+@app.route('/addrow', methods=['POST', 'GET', 'HEAD'])
+def insert():
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # not too how to test if this works
+    page = request.referrer
+    entry = Pageloads(timestamp = datetime.today(), page_name = page)
+    session.add(entry)
+
+    try:
+        session.commit()
+    except Exception as e:
+        session.rollback()
+        print(e)
+    finally:
+        session.close()
+
+    return '', 204
+
+if __name__ == '__main__':
+    app.run(debug=True)
