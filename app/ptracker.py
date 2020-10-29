84809fc8fafa053273eb22eaf6ada9b896b70e08
Michael Hardy <michardy@users.noreply.github.com>
2020-10-28 21:16:43 -0700

    Make /addrow return a proper PNG

--- app/ptracker.py
+++ app/ptracker.py
@@ -3,11 +3,9 @@ app = Flask(__name__)
 
 from db import engine, Pageloads
 from sqlalchemy.orm import sessionmaker
-from datetime import datetime, timedelta
+from datetime import datetime, timedeltaz
 
-# Add URL handling functions here
-# API handlers should probably go under /api/
-# tracking shout probably go under something else
+from pixel import PIXEL
 
 # TODO: Input company name
 @app.route('/addrow', methods=['POST', 'GET', 'HEAD'])
@@ -30,7 +28,7 @@ def insert():
     finally:
         session.close()
 
-    return '', 204
+    return(PIXEL , mimetype='application/png')
 
 
 # query db and return list of pageloads
