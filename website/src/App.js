8c4784829071edd00dd59c836c469a12a8a585dd
AE86 <a314b15926c@gmail.com>
2020-11-12 18:09:50 -0800

    Integrated company visits table into the website

--- website/src/App.js
+++ website/src/App.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from "react";
-import DatePicker from "react-datepicker"; 
-import Pageloads from "./components/pageloads";
+import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import './App.css';
+
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Components
+import Pageloads from "./components/pageloads";
+import PageloadsTable from "./components/pageloads_table";
 
 
 function App() {
@@ -10,8 +16,6 @@ function App() {
   const [startDate, setStartDate] = useState(new Date(endDate.getTime() - (10*24*60*60*1000)));
   endDate.setHours(23, 59, 59, 999);
   startDate.setHours(0, 0, 0, 0);
-  
-  const [visits, setVisits] = useState([]);
   
   let [, startDay, , startYear, startTime] = startDate.toUTCString().split(" ");
   let startMonth = startDate.getUTCMonth() + 1;
@@ -24,11 +28,20 @@ function App() {
   let startDateEncoded = encodeURIComponent(startDateString);
   let endDateEncoded = encodeURIComponent(endDateString);
 
+  const [visits, setVisits] = useState([]);
+  const [visitsPerCompany, setVisitsPerCompany] = useState([]);
+
   useEffect(() => {
-    fetch("/pageloads?start_date=" + startDateEncoded + "&end_date=" + endDateEncoded).then(response => 
-      response.json().then(data => {
+    fetch("/pageloads?start_date=" + startDateEncoded + "&end_date=" + endDateEncoded)
+    .then(response => response.json())
+    .then(data => {
         setVisits(data);
-      }))
+    })
+    fetch("/pageloads_per_company")
+    .then(response => response.json())
+    .then(data => {
+      setVisitsPerCompany(data);
+    })
   }, [startDateEncoded, endDateEncoded]);
 
   console.log("start date: " + startDate.toString());
@@ -57,6 +70,7 @@ function App() {
           minDate={startDate}
         />
         <Pageloads pageloads={visits} />
+        <PageloadsTable pageloadsPerCompany={visitsPerCompany} />
       </div>
       {/* <p>
         { startDate.toUTCString() } <br/>
