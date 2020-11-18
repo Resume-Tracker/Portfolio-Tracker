109f35240288714d079608737efac667566220ad
timle3 <timothyle0303@gmail.com>
2020-11-18 04:55:48 -0800

    Date range for table and refractored js code

--- website/src/App.js
+++ website/src/App.js
@@ -1,96 +1,53 @@
-import React, { useState, useEffect } from "react";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import './App.css';
-
-// Bootstrap
-import Container from 'react-bootstrap/Container';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState } from 'react'
+import 'react-datepicker/dist/react-datepicker.css'
+import './App.css'
 
 // Components
-import Pageloads from "./components/pageloads";
-import PageloadsTable from "./components/pageloads_table";
+import Pageloads from './components/pageloads'
+import PageloadsTable from './components/pageloads_table'
+import DateRange from './components/DateRange'
 
+function App () {
+  const [endDate, setEndDate] = useState(new Date())
+  const [startDate, setStartDate] = useState(new Date(endDate.getTime() - (10 * 24 * 60 * 60 * 1000)))
+  endDate.setHours(23, 59, 59, 999)
+  startDate.setHours(0, 0, 0, 0)
 
-function App() {
-  const [endDate, setEndDate] = useState(new Date());
-  const [startDate, setStartDate] = useState(new Date(endDate.getTime() - (10*24*60*60*1000)));
-  endDate.setHours(23, 59, 59, 999);
-  startDate.setHours(0, 0, 0, 0);
-  
-  let [, startDay, , startYear, startTime] = startDate.toUTCString().split(" ");
-  let startMonth = startDate.getUTCMonth() + 1;
-  let startDateString = startYear + "-" + startMonth + "-" + startDay + " " + startTime;
-   
-  let [, endDay, , endYear, endTime] = endDate.toUTCString().split(" ");
-  let endMonth = endDate.getUTCMonth() + 1;
-  let endDateString = endYear + "-" + endMonth + "-" + endDay + " " + endTime;
+  const [, startDay, , startYear, startTime] = startDate.toUTCString().split(' ')
+  const startMonth = startDate.getUTCMonth() + 1
+  const startDateString = startYear + '-' + startMonth + '-' + startDay + ' ' + startTime
 
-  let startDateEncoded = encodeURIComponent(startDateString);
-  let endDateEncoded = encodeURIComponent(endDateString);
+  const [, endDay, , endYear, endTime] = endDate.toUTCString().split(' ')
+  const endMonth = endDate.getUTCMonth() + 1
+  const endDateString = endYear + '-' + endMonth + '-' + endDay + ' ' + endTime
 
-  const [visits, setVisits] = useState([]);
-  const [visitsPerCompany, setVisitsPerCompany] = useState([]);
-
-  useEffect(() => {
-    fetch("/pageloads?start_date=" + startDateEncoded + "&end_date=" + endDateEncoded)
-    .then(response => response.json())
-    .then(data => {
-        setVisits(data);
-    })
-    fetch("/pageloads_per_company")
-    .then(response => response.json())
-    .then(data => {
-      setVisitsPerCompany(data);
-    })
-  }, [startDateEncoded, endDateEncoded]);
-
-  console.log("start date: " + startDate.toString());
-  console.log("end date: " + endDate.toString());
-  console.log(visits);
+  const startDateEncoded = encodeURIComponent(startDateString)
+  const endDateEncoded = encodeURIComponent(endDateString)
 
   return (
-    <div className="App">
-      <div className="App-header">
+    <div className='App'>
+      <div className='App-header'>
         <h1>Portfolio Tracker</h1>
       </div>
-      <div className="App-body">
-        <Container>
-          <DatePicker
-            selected={startDate}
-            onChange={date => setStartDate(date)}
-            selectsStart
-            startDate={startDate}
-            endDate={endDate}
-          />
-          <br />
-          <DatePicker
-            selected={endDate}
-            onChange={date => setEndDate(date)}
-            selectsEnd
-            startDate={startDate}
-            endDate={endDate}
-            minDate={startDate}
-          />
-          <br />
-          <Pageloads pageloads={visits} />
-          <br />
-          <PageloadsTable pageloadsPerCompany={visitsPerCompany} />
-        </Container>
+      <div className='App-body'>
+        <DateRange
+          startDate={startDate}
+          endDate={endDate}
+          setStartDate={setStartDate}
+          setEndDate={setEndDate}
+        />
+        <Pageloads
+          encodedStartDate={startDateEncoded}
+          encodedEndDate={endDateEncoded}
+        />
+        <PageloadsTable
+          encodedStartDate={startDateEncoded}
+          encodedEndDate={endDateEncoded}
+        />
       </div>
-      {/* <p>
-        { startDate.toUTCString() } <br/>
-        { startDateString } <br/>
-        { startDateEncoded }
-      </p>
 
-      <p>
-        { endDate.toUTCString() } <br/>
-        { endDateString } <br/>
-        { endDateEncoded }
-      </p> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- website/src/components/pageloads.js
+++ website/src/components/pageloads.js
@@ -1,61 +1,69 @@
-import React from 'react';
-import {ResponsiveContainer, ComposedChart, Line, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+import React, { useState, useEffect } from 'react'
+import { ResponsiveContainer, ComposedChart, Line, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
 
-export const Pageloads = ({ pageloads }) => {
+const fetch = require('node-fetch')
+
+export const Pageloads = ({ encodedStartDate, encodedEndDate }) => {
+  const [pageloads, setPageloads] = useState([])
+
+  useEffect(() => {
+    fetch('/pageloads?start_date=' + encodedStartDate + '&end_date=' + encodedEndDate)
+      .then(response => response.json())
+      .then(data => {
+        setPageloads(data)
+      })
+  }, [encodedStartDate, encodedEndDate])
+
   // convert timestamps into local time format (mm/dd/yyyy) for the graph
-  let timestamps = pageloads.map(d => {
-    const date = new Date(d.timestamp);
-    return `${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`;
+  const timestamps = pageloads.map(d => {
+    const date = new Date(d.timestamp)
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
   })
 
   // count the number of occurences for each timestamp
-  let occurrences = {};
-  for (var i = 0; i < timestamps.length; i++) {
-    occurrences[timestamps[i]] = (occurrences[timestamps[i]] || 0) + 1;
+  const occurrences = {}
+  for (let i = 0; i < timestamps.length; i++) {
+    occurrences[timestamps[i]] = (occurrences[timestamps[i]] || 0) + 1
   }
 
   // put ouccurences into data format
-  let dates = Object.keys(occurrences);
-  let visits = Object.values(occurrences);
-  var data = [];
-  for (i = 0; i < Object.keys(occurrences).length; i++) {
-    var temp = {};
-    temp.date = dates[i];
-    temp.visits = visits[i];
-    data.push(temp);
+  const dates = Object.keys(occurrences)
+  const visits = Object.values(occurrences)
+  const data = []
+  for (let i = 0; i < Object.keys(occurrences).length; i++) {
+    const temp = {}
+    temp.date = dates[i]
+    temp.visits = visits[i]
+    data.push(temp)
   }
-
-  console.log(data);
 
   // only render the line chart if there are data
   if (Object.keys(data).length === 0) {
     return (
-      <div id="pageloads">
-      </div>
-    );
-  } 
-  else {
+      <div class='pageloads' />
+    )
+  } else {
     return (
-      <div id="pageloads">
-        <ResponsiveContainer width="100%" height={400}>
+      <div className='pageloads'>
+        <ResponsiveContainer width='100%' height={400}>
           <ComposedChart
             data={data}
             margin={{
-              top: 20, right: 20, bottom: 20, left: 20,
+              top: 25, right: 25, bottom: 25, left: 25
             }}
           >
-          <CartesianGrid stroke="#f5f5f5" />
-          <XAxis dataKey="date" allowDuplicatedCategory={false}/>
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="visits" barSize={10} fill="#413ea0" />
-          <Line type="monotone" dataKey="visits" stroke="#ff7300" legendType="none"/>
+            <CartesianGrid stroke='#f5f5f5' />
+            <XAxis dataKey='date' allowDuplicatedCategory={false} />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey='visits' barSize={10} fill='#413ea0' />
+            <Line type='monotone' dataKey='visits' stroke='#ff7300' legendType='none' />
           </ComposedChart>
         </ResponsiveContainer>
       </div>
-    );
+    )
   }
-};
+}
 
-export default Pageloads;
+export default Pageloads
