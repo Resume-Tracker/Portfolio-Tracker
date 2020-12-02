95d3cfa6572afce25d980c475c5fa4ce1c0b93ed
AE86 <a314b15926c@gmail.com>
2020-12-01 20:41:48 -0800

    replaced tabs with spaces in index.js

--- website/src/index.js
+++ website/src/index.js
@@ -8,8 +8,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-  	<Router>
-    	<App />
+    <Router>
+      <App />
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
